cf20c7ca8dbe7fd6884532b32401a4ae03e4ab93
flyingsl0ths <randomemail33110@protonmail.com>
2024-03-15 17:46:34 -0700

    Add project files

---
+++ main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const math = std.math;
+
+fn map(value: f32, from: struct { f32, f32 }, to: struct { f32, f32 }) f32 {
+    const mapped = ((value - from[0]) * (to[1] - to[0])) / (from[1] - from[0]) + to[0];
+    return math.min(math.max(mapped, to[0]), to[1]);
+}
+
+fn escapeTime(c: @TypeOf(math.Complex(f32))) ?u32 {
+    var z = math.Complex(f32){ .re = 0, .im = 0 };
+
+    const max_iterations = 1000;
+
+    for (0..max_iterations) |i| {
+        if (z.magnitude() > 4.0) {
+            return i;
+        }
+
+        z = z * z + c;
+    }
+}
+
+pub fn main() !void {
+    _ = std.io.getStdOut().write("Hello World!!");
+}
